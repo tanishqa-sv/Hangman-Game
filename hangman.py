0e0a28430b4cdccc0c078abae5e4ce6ec6041541
akanksha1999 <47079315+akanksha1999@users.noreply.github.com>
2020-04-28 11:14:31 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,6 +1,13 @@
 from tkinter import *
+from string import ascii_uppercase
 
 window=Tk()
 window.title("Hangman Game")
+
+#keyboard letter buttons :
+n=0
+for c in ascii_uppercase :
+	Button(window, text=c,width=4,command= lambda letr=c : guess(letr), font=('Helvetica' ,'18')).grid(row=1+ n//9,column= n%9)
+	n=n+1
 
 window.mainloop()
