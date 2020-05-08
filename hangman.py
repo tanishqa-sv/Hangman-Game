25cc8707ab142c97e49ca2f9f480ad9cbd3f3110
akanksha1999 <47079315+akanksha1999@users.noreply.github.com>
2020-05-08 17:50:37 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -4,6 +4,13 @@ from string import ascii_uppercase
 window=Tk()
 window.title("Hangman Game")
 
+#an array of images to be used with indices :
+photo=[PhotoImage(file=r"images\0.png"), PhotoImage(file=r"images\1.png"),  PhotoImage(file=r"images\2.png"),  PhotoImage(file=r"images\3.png"),  PhotoImage(file=r"images\4.png"),  PhotoImage(file=r"images\5.png"),  PhotoImage(file=r"images\6.png")] 
+
+imgLabel=Label(window)                                                                        #a label for images
+imgLabel.grid(row=0,column=0,columnspan=3,pady=40)
+imgLabel.config(image=photo[0])				#assigning initial image to label 
+
 #keyboard letter buttons :
 n=0
 for c in ascii_uppercase :
