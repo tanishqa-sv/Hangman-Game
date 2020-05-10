4008ca5f81aa0c0da6bb916664170cda2a00f818
Tanishka Khichi <47146793+tanishqa-sv@users.noreply.github.com>
2020-05-10 11:43:07 +0530

    update blanks

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import string
 from string import ascii_uppercase
 
 window=Tk()
@@ -7,9 +8,17 @@ window.title("Hangman Game")
 #an array of images to be used with indices :
 photo=[PhotoImage(file=r"images\0.png"), PhotoImage(file=r"images\1.png"),  PhotoImage(file=r"images\2.png"),  PhotoImage(file=r"images\3.png"),  PhotoImage(file=r"images\4.png"),  PhotoImage(file=r"images\5.png"),  PhotoImage(file=r"images\6.png")] 
 
-imgLabel=Label(window)                                                                        #a label for images
-imgLabel.grid(row=0,column=0,columnspan=3,pady=40)
+imgLabel=Label(window)					#a label for images
+imgLabel.grid(row=0,column=0,columnspan=3,pady=40)			
+
 imgLabel.config(image=photo[0])				#assigning initial image to label 
+
+display_word=StringVar()
+txtLabel=Label(window, text=display_word.get(), font=('Helvetica' ,'18'))	#label displays string variable
+txtLabel.grid(column=3,columnspan=6,row=0)
+
+display_word.set(" ".join("_"*len("example_word")))			#initially set display word to blanks
+txtLabel.config(text=display_word.get())
 
 #keyboard letter buttons :
 n=0
@@ -18,3 +27,4 @@ for c in ascii_uppercase :
 	n=n+1
 
 window.mainloop()
+
